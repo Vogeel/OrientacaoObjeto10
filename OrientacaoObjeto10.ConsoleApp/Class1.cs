59fa51c9a01dc7bfcd2a08de692f94a58778517f
Vogeel <vitorvogel12@gmail.com>
2022-03-08 16:42:27 -0300

    Add project files.

---
+++ OrientacaoObjeto10.ConsoleApp/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrientacaoObjeto10.ConsoleApp
+{
+    internal class Aluno
+    {
+        public double n1, n2, p1, p2;
+        public double Media()
+        {
+            double ValorMedia = (n1 * p1 + n2 * p2) / (p1 + p2);
+            return ValorMedia;
+        }
+    }
+}
---
+++ OrientacaoObjeto10.ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OrientacaoObjeto10.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno medias = new Aluno();
+            medias.p1 = 5;
+            medias.p2 = 5;
+            
+            medias.n1 = 5;
+            medias.n2 = 6;
+           
+
+            Console.WriteLine($"A media ponderada do aluno é de: {medias.Media()}");
+            Console.ReadLine();
+        }
+    }
+}
